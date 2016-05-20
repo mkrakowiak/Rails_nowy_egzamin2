fbcec1f377160f57ca1a336fe7b84f51b8c70f58
Lukasz <lukasz922@gmail.com>
2016-05-20 16:22:07 +0000

    dodano wyglad edycji

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,24 +24,24 @@ class PostsController < ApplicationController
 	end
 
 	def edit
-	#	@post = Post.find(params[:id])
+		@post = Post.find(params[:id])
 	end
 
 	def update
-	#	@post = Post.find(params[:id])
+		@post = Post.find(params[:id])
 
-	#	if @post.update(params[:post].permit(:title, :body))
-	#		redirect_to @post
-	#	else
-	#		render 'edit'
-	#	end
+		if @post.update(params[:post].permit(:title, :body))
+			redirect_to @post
+		else
+			render 'edit'
+		end
 	end
 
 	def destroy
-#		@post = Post.find(params[:id])
-	#	@post.destroy
+		@post = Post.find(params[:id])
+		@post.destroy
 
-	#	redirect_to posts_path
+		redirect_to posts_path
 	end
 
 	private
